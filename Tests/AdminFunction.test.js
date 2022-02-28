097cc4cf5418a83450e1e37097acea6d2f34b48e
hamraz <hamriouz@gmail.com>
2022-02-28 17:41:30 +0330

    debug

--- Tests/AdminFunction.test.js
+++ Tests/AdminFunction.test.js
@@ -2,27 +2,6 @@ const Admin = require("../model/Admin")
 const {Registration} = require("../controller/Registration");
 const CreateAdmin = require("../controller/CreateAdmin");
 const User = require("../model/User");
-const e = require("express");
-
-/*test("get all employees from a department", () => {
-    Registration.createEmployeeByAdmin("a","a","a","123456789a","a","dep","a","a","9-16","a","a")
-    Registration.createEmployeeByAdmin("a","a","aa","123456789a","a","dep","a","a","9-16","a","a")
-    Registration.createEmployeeByAdmin("a","a","aaa","123456789a","a","dep","a","a","9-16","a","a")
-    Registration.createEmployeeByAdmin("a","a","aaaa","123456789a","a","depart","a","a","9-16","a","a")
-    let user = User.findObjectByKey("email", "a");
-    expect(user.get_all_employee("dep")).toStrictEqual({"dep": ["a", "aa", "aaa"]})
-    User.removeAllUsers()
-})*/
-/*test("view list of all employees", () => {
-    Registration.createAdmin("a","a","a","123456789a","a","de","a","a","9-12")
-    // Registration.createEmployeeByAdmin("a","a","a","123456789a","a","dep","a","a","9-16","a","employee")
-    Registration.createEmployeeByAdmin("a","a","aa","123456789a","a","depa","a","a","9-16","employee","enable")
-    Registration.createEmployeeByAdmin("a","a","aaa","123456789a","a","depar","a","a","9-16","employee","enable")
-    Registration.createEmployeeByAdmin("a","a","aaaa","123456789a","a","depart","a","a","9-16","employee","enable")
-    let admin = Admin.findObjectByKey("email","a");
-    // expect(admin.role).toBe("admin")
-    expect(admin.view_detail_one_employee("aaa").toStrictEqual({"dep": ["a", "aa", "aaa"]}))
-})*/
 
 test("view list of all employees", () => {
     CreateAdmin.createAdmin(undefined, "a","a","a","123456789a","1","q","a","a","9-12");
@@ -84,7 +63,7 @@ test("view detail of one employee", () => {
             "Organization Level": "a",
             "Phone Number": "a",
             "Role": "employee",
-            "Status": "employee",
+            "Status": "enable",
             "Working Hour": "8-15"
         }
     })
@@ -98,4 +77,5 @@ test("enable disable employee", () => {
     let employee = User.findObjectByKey("email", "aa")
     admin.enable_disable("aa");
     expect(employee.status).toBe("disable")
+    User.removeAllUsers()
 })
